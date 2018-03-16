ef333332b62f64ef2157695be08c788729281a9b
Brianna''Kincaid <bkincaid@bu.edu>
2018-03-16 14:34:21 -0400

    part 1

--- Part_1.R
+++ Part_1.R
@@ -27,7 +27,6 @@ yr2004 <- read_table2("46035 Data/46035h2004.txt")
 yr2005 <- read_table2("46035 Data/46035h2005.txt")
 yr2006 <- read_table2("46035 Data/46035h2006.txt")
 yr2007 <- read_table2("46035 Data/46035h2007.txt", skip = 2, col_names = c("YY", "MM", "DD", "hh", "mm", "WDIR", "WSPD", "GST",  "WVHT",   "DPD",   "APD", "MWD",   "PRES",  "ATMP",  "WTMP",  "DEWP",  "VIS", "TIDE"))
-
 yr2008 <- read_table2("46035 Data/46035h2008.txt", skip = 2, col_names = c("YY", "MM", "DD", "hh", "mm", "WDIR", "WSPD", "GST",  "WVHT",   "DPD",   "APD", "MWD",   "PRES",  "ATMP",  "WTMP",  "DEWP",  "VIS", "TIDE"))
 yr2009 <- read_table2("46035 Data/46035h2009.txt", skip = 2, col_names = c("YY", "MM", "DD", "hh", "mm", "WDIR", "WSPD", "GST",  "WVHT",   "DPD",   "APD", "MWD",   "PRES",  "ATMP",  "WTMP",  "DEWP",  "VIS", "TIDE"))
 yr2010 <- read_table2("46035 Data/46035h2010.txt", skip = 2, col_names = c("YY", "MM", "DD", "hh", "mm", "WDIR", "WSPD", "GST",  "WVHT",   "DPD",   "APD", "MWD",   "PRES",  "ATMP",  "WTMP",  "DEWP",  "VIS", "TIDE"))
@@ -47,13 +46,177 @@ yr2017 <- read_table2("46035 Data/46035h2017.txt", skip = 2, col_names = c("YY",
 
 ## TRANSFORM/ORGANIZE (dplyr)
 
+simple_85 <- yr1985 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP) %>%
+  filter(WTMP != 999)
+
+simple_86 <- yr1986 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_87 <- yr1987 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_88 <- yr1988 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_89 <- yr1989 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_90 <- yr1990 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_91 <- yr1991 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_92 <- yr1992 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_93 <- yr1993 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_94 <- yr1994 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_95 <- yr1995 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_96 <- yr1996 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_97 <- yr1997 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_98 <- yr1998 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_99 <- yr1999 %>%
+  filter(hh == 12) %>%
+  separate(YYYY, into = c("CC", "YY"), sep = 2) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_00 <- yr2000 %>%
+  filter(hh == 12) %>%
+  separate(YYYY, into = c("CC", "YY"), sep = 2) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_01 <- yr2001 %>%
+  filter(hh == 12) %>%
+  separate(YYYY, into = c("CC", "YY"), sep = 2) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_02 <- yr2002 %>%
+  filter(hh == 12) %>%
+  separate(YYYY, into = c("CC", "YY"), sep = 2) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_03 <- yr2003 %>%
+  filter(hh == 12) %>%
+  separate(YYYY, into = c("CC", "YY"), sep = 2) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_04 <- yr2004 %>%
+  filter(hh == 12) %>%
+  separate(YYYY, into = c("CC", "YY"), sep = 2) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_05 <- yr2005 %>%
+  filter(hh == 12) %>%
+  separate(YYYY, into = c("CC", "YY"), sep = 2) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_06 <- yr2006 %>%
+  filter(hh == 12) %>%
+  separate(YYYY, into = c("CC", "YY"), sep = 2) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_07 <- yr2007 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_08 <- yr2008 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_09 <- yr2009 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_10 <- yr2010 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_11 <- yr2011 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_12 <- yr2012 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_14 <- yr2014 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_15 <- yr2015 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_16 <- yr2016 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+simple_17 <- yr2017 %>%
+  filter(hh == 12) %>%
+  select(YY, MM, DD, ATMP, WTMP)
+
+all_awtmp <- rbind(simple_85, simple_86, simple_87, simple_88, simple_89, simple_90, simple_91, simple_92, simple_93, simple_94, simple_95, simple_96, simple_97, simple_98, simple_99, simple_00, simple_01, simple_02, simple_03, simple_04, simple_05, simple_06, simple_07, simple_08, simple_09, simple_10, simple_11, simple_12, simple_14, simple_15, simple_16, simple_17)
+all_awtmp <- all_awtmp %>%
+  unite(day, MM, DD, YY, sep = "/")
+
+all_awtmp$ATMP <- as.numeric(all_awtmp$ATMP)
+all_awtmp$ATMP <- as.numeric(all_awtmp$WTMP)
+
+all_awtmp2 <- all_awtmp %>%
+  filter(ATMP != 999.0 & WTMP != 999.0)
+
 ## VISUALIZE/EXPLORE -- Questions, Variation, Missing Values, Covariation, Patterns and Models  (ggplot2)
 
-    # time series composed of 30 years of daily Air Temperature and Sea Temperature readings recorded at noon for the time zone of Station 46035.
-    # look at relationship between air temperature and sea temperature. are they related?
-    # Some data may be missing. What should you do about missing data? See “An introduction to data cleaning with R” by Edwin de Jonge and Mark van der Loo in the Contributed Documentation section at the bottom of the CRAN R page.
-    # Has the mean temperature changed over the past 30 years? What statistical methods can you use to test if the change is statistically significant?
-    # You been instructed to use only one sample per day day out of 24 daily hourly temperature readings. Has your sampling affected your evaluation of temperature change? In what way? Explain and demonstrate.
+# time series composed of 30 years of daily Air Temperature and Sea Temperature readings recorded at noon for the time zone of Station 46035.
+
+ggplot(data = all_awtmp2) +
+  geom_point(mapping = aes(ATMP, WTMP))
+
+ggplot(data = all_awtmp) +
+  geom_histogram(mapping = aes(x = ATMP), stat = "count", binwidth = 100)
+
+ggplot(data = all_awtmp2) + 
+  geom_line(mapping = aes(x = ATMP, y = day))
+                 
+# look at relationship between air temperature and sea temperature. are they related?
+
+
+
+# Some data may be missing. What should you do about missing data? See “An introduction to data cleaning with R” by Edwin de Jonge and Mark van der Loo in the Contributed Documentation section at the bottom of the CRAN R page.
+
+# Has the mean temperature changed over the past 30 years? What statistical methods can you use to test if the change is statistically significant?
+
+# You been instructed to use only one sample per day day out of 24 daily hourly temperature readings. Has your sampling affected your evaluation of temperature change? In what way? Explain and demonstrate.
+
+
 
 ## SLIDES AND SHINY
 
